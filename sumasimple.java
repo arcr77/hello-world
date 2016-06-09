3b24cb5e2013713a8d92a5e259dbdd428d130374
arcr77 <arcr77@gmail.com>
2016-06-09 10:14:07 +0000

    Add files via upload

---
+++ sumasimple.java
@@ -0,0 +1,21 @@
+
+/**
+ * Write a description of class uno here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class sumasimple
+{
+    public static void main (String [] args)
+    {
+    int a=1;
+    
+    System.out.println("el valor de a=" +a);
+    a=a+10;
+    System.out.println("ahora el valor de a es="+a);
+
+   
+        
+    }
+}
